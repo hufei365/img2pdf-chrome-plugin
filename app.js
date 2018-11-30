33f029c4449dd13d91ad7040c22a9cde632816d0
hufei <hufeicom@qq.com>
2018-11-30 17:29:59 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,1 @@
+chrome.tabs.create({ url: '/bg.html' }, function(tab){});
---
+++ bg.js
@@ -0,0 +1,40 @@
+const file = document.getElementById('file');
+
+let count = 0;
+
+const fix = document.getElementsByClassName('fix')[0];
+
+function append(file){
+    let reader = new FileReader();
+    let img = new Image();
+
+    reader.onload=(e)=>{
+        img.src = e.target.result;
+        document.body.appendChild(img);
+        if(--count === 0){
+            setTimeout(()=>{
+                window.print();
+            }, 200);
+        }
+    }
+    reader.readAsDataURL(file);
+}
+window.onbeforeprint = ()=>{
+    fix.hidden=true;
+    document.body.className="print"
+}
+window.onafterprint = ()=>{
+    fix.hidden=false;
+    document.body.className=""
+}
+
+
+
+file.addEventListener('input', (event)=>{
+    
+    document.title = file.files[0].name.replace(/\.[pngje]+$/g, '');
+    count = file.files.length; 
+    Array.prototype.map.call(file.files, (f)=>{
+        append(f);
+    });
+});
